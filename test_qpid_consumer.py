b3f112ca1a101d1af7a8045ab8943be0eb1309b5
Russell Bryant <rbryant@redhat.com>
2012-01-05 11:04:39 -0500

    Add beginning of a consumer side test driver.
    Change-Id: I1cbade4bcaf8e75d3f06733827643ab3974bdf3d

---
+++ test_qpid_consumer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+from nova import rpc
+from nova import flags
+
+flags.FLAGS['rpc_backend'].SetDefault('nova.rpc.impl_qpid')
+
+
+class PingConsumer(object):
+    def __init__(self):
+        pass
+
+    def ping(self):
+        print "Got a ping"
+        return "pong"
+
+    def ping_noreply(self):
+        print "Got a ping (no reply)"
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    consumer = PingConsumer()
+
+    conn = rpc.create_connection(new=True)
+    conn.create_consumer("impl_qpid_test", consumer, fanout=False)
+    conn.consume_in_thread()
+
+    while True:
+        try:
+            time.sleep(500)
+        except KeyboardInterrupt:
+            break
+
+    conn.close()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
