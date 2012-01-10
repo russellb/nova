992bd7a3784087a14cbb096d0895a7b64974dbd9
Russell Bryant <rbryant@redhat.com>
2012-01-10 12:22:14 -0500

    Add some impl_qpid unit tests
    Change-Id: I391a047ca78e29d10331b852be7447fb195de469

--- nova/tests/rpc/test_qpid.py
+++ nova/tests/rpc/test_qpid.py
@@ -20,9 +20,8 @@
 Unit Tests for remote procedure calls using qpid
 """
 
-import eventlet
-
-eventlet.monkey_patch()
+import mox
+import qpid
 
 from nova import context
 from nova import log as logging
@@ -34,10 +33,121 @@ from nova.tests.rpc import common
 LOG = logging.getLogger('nova.tests.rpc')
 
 
-class RpcQpidTestCase(common._BaseRpcTestCase):
+class RpcQpidTestCase(test.TestCase):
     def setUp(self):
-        self.rpc = impl_qpid
+        self.mocker = mox.Mox()
         super(RpcQpidTestCase, self).setUp()
 
     def tearDown(self):
+        self.mocker.ResetAll()
         super(RpcQpidTestCase, self).tearDown()
+
+    def test_create_connection(self):
+        mock_connection = self.mocker.CreateMock(qpid.messaging.Connection)
+        mock_session = self.mocker.CreateMock(qpid.messaging.Session)
+
+        mock_connection.opened().AndReturn(False)
+        mock_connection.open()
+        mock_connection.session().AndReturn(mock_session)
+        mock_connection.close()
+
+        def _fake_connection(*args, **kwargs):
+            return mock_connection
+
+        def _fake_session(*args, **kwargs):
+            return mock_session
+
+        orig_connection = qpid.messaging.Connection
+        qpid.messaging.Connection = _fake_connection
+        orig_session = qpid.messaging.Session
+        qpid.messaging.Session = _fake_session
+
+        self.mocker.ReplayAll()
+
+        connection = impl_qpid.create_connection()
+        connection.close()
+
+        self.mocker.VerifyAll()
+
+        qpid.messaging.Connection = orig_connection
+        qpid.messaging.Session = orig_session
+
+    def _test_create_consumer(self, fanout):
+        mock_connection = self.mocker.CreateMock(qpid.messaging.Connection)
+        mock_session = self.mocker.CreateMock(qpid.messaging.Session)
+        mock_receiver = self.mocker.CreateMock(qpid.messaging.Receiver)
+
+        mock_connection.opened().AndReturn(False)
+        mock_connection.open()
+        mock_connection.session().AndReturn(mock_session)
+        if fanout:
+            # The link name includes a UUID, so match it with a regex.
+            expected_address = mox.Regex(r'^impl_qpid_test_fanout ; '
+                '{"node": {"x-declare": {"auto-delete": true, "durable": '
+                'false, "type": "fanout"}, "type": "topic"}, "create": '
+                '"always", "link": {"x-declare": {"auto-delete": true, '
+                '"durable": false}, "durable": true, "name": '
+                '"impl_qpid_test_fanout_.*"}}$')
+        else:
+            expected_address = 'nova/impl_qpid_test ; {"node": {"x-declare": '\
+                '{"auto-delete": true, "durable": true}, "type": "topic"}, ' \
+                '"create": "always", "link": {"x-declare": {"auto-delete": ' \
+                'true, "exclusive": false, "durable": false}, "durable": ' \
+                'true, "name": "impl_qpid_test"}}'
+        mock_session.receiver(expected_address).AndReturn(mock_receiver)
+        mock_receiver.capacity = 1
+        mock_connection.close()
+
+        def _fake_connection(*args, **kwargs):
+            return mock_connection
+
+        def _fake_session(*args, **kwargs):
+            return mock_session
+
+        def _fake_receiver(*args, **kwargs):
+            return mock_receiver
+
+        orig_connection = qpid.messaging.Connection
+        qpid.messaging.Connection = _fake_connection
+        orig_session = qpid.messaging.Session
+        qpid.messaging.Session = _fake_session
+        orig_receiver = qpid.messaging.Receiver
+        qpid.messaging.Session = _fake_session
+
+        self.mocker.ReplayAll()
+
+        def _proxy(*args, **kwargs):
+            pass
+
+        connection = impl_qpid.create_connection()
+        consumer = connection.create_consumer("impl_qpid_test",
+                                              lambda *_x, **_y: None,
+                                              fanout=fanout)
+        connection.close()
+
+        self.mocker.VerifyAll()
+
+        qpid.messaging.Connection = orig_connection
+        qpid.messaging.Session = orig_session
+        qpid.messaging.Receiver = orig_receiver
+
+    def test_create_consumer(self):
+        self._test_create_consumer(fanout=False)
+
+    def test_create_consumer_fanout(self):
+        self._test_create_consumer(fanout=True)
+
+#
+# Qpid does not have a handy in-memory transport like kombu, so it's not
+# terribly straight forward to take advantage of the common unit tests.
+# However, at least at the time of this writing, the common unit tests all pass
+# with qpidd running.
+#
+# class RpcQpidCommonTestCase(common._BaseRpcTestCase):
+#     def setUp(self):
+#         self.rpc = impl_qpid
+#         super(RpcQpidCommonTestCase, self).setUp()
+#
+#     def tearDown(self):
+#         super(RpcQpidCommonTestCase, self).tearDown()
+#
